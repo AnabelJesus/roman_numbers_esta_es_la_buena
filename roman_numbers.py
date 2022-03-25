cc7f8c9cee5a3983d4d1f926722aa2749467d32e
Anabel <anabel.jesus@grupotrebolenergia.es>
2022-03-25 14:19:08 +0100

    test en verde

--- roman_numbers.py
+++ roman_numbers.py
@@ -1,7 +1,21 @@
+
+
+
+
 class roman_numbers:
 
     def __init__(self):
         pass
 
     def roman(self,input):
-        return "I"
+        if input == 1:
+            return "I"
+
+        if input == 5:
+            return "V"
+
+        if input == 10:
+            return "X"
+
+        if input == 50:
+            return "L"
